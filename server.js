9c43da974b8d98bd852bf5c2bd2e0bd1237d3160
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-02-19 12:18:33 -0800

    Updating server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// Load the HTTP library
+	// Load the HTTP library
 var http = require("http");
 
 // Create an HTTP server to handle responses
-http.creatServer( function(req, res){
+http.createServer( function(req, res){
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.write("Sup Foo");
 	res.end();
